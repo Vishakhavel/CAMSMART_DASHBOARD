f2082eb4ff3334e132a3e5a7f13b6611d2db5890
vichu <vichu2grand@gmail.com>
2021-09-15 12:35:31 +0530

    sony mods

--- src/api/dashboard/professor.js
+++ src/api/dashboard/professor.js
@@ -4,14 +4,14 @@ class professor{
 
   ExecuteProfessorService(){
     console.log("API CALL TO DB")
-    return axios.get('http://13.232.44.96:8080/rest/users/all');
+    return axios.get('http://3.109.182.143:8080/rest/users/all');
     /*return axios.get('g');*/
   }
 
   ExecuteProfessorDeleteService(id){
     console.log("API DELETE CALL TO DB")
     /*axios.delete('http://localhost:8080/rest/users/delete/${id}');*/
-    axios.delete('http://13.232.44.96:8080/rest/users/delete/${id}'); /*13.232.44.96*/
+    axios.delete('http://3.109.182.143:8080/rest/users/delete/${id}'); /*13.232.44.96*/
   }
 
 
